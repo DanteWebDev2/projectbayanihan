3f40c40f72152ed6df4dbbbb292809642edcf224
joymee <joymee.dante@gsfe.tupcavite.edu.ph>
2021-04-03 12:46:42 +0800

    add

---
+++ myWEB/tests.py
@@ -0,0 +1,19 @@
+from django.urls import resolve
+from django.test import TestCase
+from myWEB.views import Mainpage
+
+from django.http import HttpRequest
+
+class HomePageTest(TestCase):
+
+	def test root_url_resolve_to_mainpage_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, MainPage)
+
+	#def test_mainpage_return_correct_view(self):
+		#request = HttpRequest()
+		#response = MainPage(request)
+		#html = response.content.decode('utf8')
+		#self.assertTrue(html.startswitch('<html>'))
+		#self.assertIn('<title>my WEB</title>', html)
+		#self.assertTrue(html.endswitch('</html>'))
