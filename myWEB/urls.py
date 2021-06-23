f75403c459343c58fbc2a0de44bfd093324b5a83
joymee <joymee.dante@gsfe.tupcavite.edu.ph>
2021-06-23 14:17:58 +0800

    June23

--- myWEB/urls.py
+++ myWEB/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
 	path('success', views.sponsor, name="success"), 
 	path('done', views.secondpage, name="done"),
 	path('nice', views.donation, name="nice"),
-	#path('person', views.mydonors, name="person"),
-	# path('success', views.Nextpage, name="success"),
-	#path('success', views.option, name="success"),
+	
 ]
--- myWEB/views.py
+++ myWEB/views.py
@@ -4,22 +4,25 @@ from .models import Sponsor, Option, Monetary, Transaction, Inkind, Settlement, 
 
 def main(request):
 
+
 	return render(request,'main.html')
 
 
 def firstpage(request):
 
+
+
 	return render(request,'firstpage.html')
 
 def sponsor(request):
 
-	'''viewer=Sponsor.objects.create(
+	viewer=Sponsor.objects.create(
 		First = request.POST['First'],
 		Surname = request.POST['Surname'],
 		Email = request.POST['Email'],
 		Contact = request.POST['Contact'],
 		Address = request.POST['Address'],
-		)'''
+		)
 
 	return render(request,'donationtype.html')
 	#return redirect('success')
@@ -27,102 +30,66 @@ def sponsor(request):
 
 def secondpage(request):
 
+	viewer=Monetary.objects.create(
+		List = request.POST['List'],
+		Amount = request.POST['Amount'],
+		Gcash = request.POST['Gcash'],
+		Bank = request.POST['Bank'],
+		Bills = request.POST['Bills'],
+		)
+
+	viewer=Transaction.objects.create(
+		Place = request.POST['Place'],
+		Fee = request.POST['Fee'],
+		)
+
+	viewer=Inkind.objects.create(
+		Food = request.POST['Food'],
+		Drinks = request.POST['Drinks'],
+		Sanitation = request.POST['Sanitation'],
+		Cure = request.POST['Cure'],
+		Count = request.POST['Count'],
+		Count1 = request.POST['Couunt1'],
+		Count2 = request.POST['Count2'],
+		Count3 = request.POST['Count3'],
+		)
+
+	viewer=Settlement.objects.create(
+		Locations = request.POST['Locations'],
+		Places = request.POST['Places'],
+		Charges = request.POST['Charges'],
+		)
+
 	return render(request,'fundraising.html') 
 
 def donation(request):
 
+	viewer=Fundraising.objects.create(
+		Organization = request.POST['Organization'],
+		Organizer = request.POST['Organizer'],
+		Event = request.POST['Event'],
+		Area = request.POST['Area'],
+		Email = request.POST['Email'],
+		Time = request.POST['Time'],
+		Item = request.POST['Item'],
+		Sum = request.POST['Sum'],
+		)
+
 	return render(request,'recipient.html')
 
-# def option(request):
+def donates(request):
 
-# 	'''viewer=Option.objects.create(
-# 		choose = request.POST[('Monetary Donation', 'Monetary Donation'), ('In-Kind Donation', 'In-Kind Donation') ('Both', 'Both')],
-# 		#Donors = request.POST['Donors'],
-
-# 		)'''
-
-# 	return render(request,'fundraising.html')
-# 	#return redirect('success')
-
-# def monetary(request):
-
-# 	'''viewer=Sponsor.objects.create(
-# 		First = request.POST['First'],
-# 		Surname = request.POST['Surname'],
-# 		Email = request.POST['Email'],
-# 		Contact = request.POST['Contact'],
-# 		Address = request.POST['Address'],
-# 		)'''
-
-# 	return render(request,'donationtype.html')
-# 	#return redirect('success')
-
-
-# def transaction(request):
-
-# 	'''viewer=Sponsor.objects.create(
-# 		First = request.POST['First'],
-# 		Surname = request.POST['Surname'],
-# 		Email = request.POST['Email'],
-# 		Contact = request.POST['Contact'],
-# 		Address = request.POST['Address'],
-# 		)'''
-
-# 	return render(request,'donationtype.html')
-# 	#return redirect('success')
-
-
-# def inkind(request):
-
-# 	'''viewer=Sponsor.objects.create(
-# 		First = request.POST['First'],
-# 		Surname = request.POST['Surname'],
-# 		Email = request.POST['Email'],
-# 		Contact = request.POST['Contact'],
-# 		Address = request.POST['Address'],
-# 		)'''
-
-# 	return render(request,'donationtype.html')
-# 	#return redirect('success')
-
-# def settlement(request):
-
-# 	'''viewer=Sponsor.objects.create(
-# 		First = request.POST['First'],
-# 		Surname = request.POST['Surname'],
-# 		Email = request.POST['Email'],
-# 		Contact = request.POST['Contact'],
-# 		Address = request.POST['Address'],
-# 		)'''
-
-# 	return render(request,'donationtype.html')
-# 	#return redirect('success')'''
-
-def fundraising(request):
-
-	'''viewer=Sponsor.objects.create(
-		First = request.POST['First'],
-		Surname = request.POST['Surname'],
-		Email = request.POST['Email'],
+	viewer=Recipient.objects.create(
+		Receiver = request.POST['Receiver'],
+		Dates = request.POST['Dates'],
+		Persons = request.POST['Person'],
 		Contact = request.POST['Contact'],
-		Address = request.POST['Address'],
-		)'''
+		Beneficiaries = request.POST['Beneficiaries'],
+	
+		)
 
-	return render(request,'fundraising.html')
-	#return redirect('success')
+	return render(request,'summary.html')
 
-def recipient(request):
-
-	'''viewer=Sponsor.objects.create(
-		First = request.POST['First'],
-		Surname = request.POST['Surname'],
-		Email = request.POST['Email'],
-		Contact = request.POST['Contact'],
-		Address = request.POST['Address'],
-		)'''
-
-	return render(request,'recipient.html')
-	#return redirect('success')
 
 
 
